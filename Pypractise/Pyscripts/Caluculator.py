ca19c43db5a1b23659f8cdb9646028bba3618579
Aditya Pratti <prattiaditya@gmail.com>
2018-03-02 17:07:44 -0700

    Caluculator Program to find Arithmetic operations

---
+++ Pypractise/Pyscripts/Caluculator.py
@@ -0,0 +1,21 @@
+# a calc program for 2 numbers 
+print("Welcome to  my calc")
+typeofop=raw_input("Would you like to add(a), substract(s), Multiply(m), Divison(d) ? ")
+if typeofop == "a":
+    x=int(raw_input("Enter the first number: "))
+    y=int(raw_input("Enter the second number:"))
+    print x + y
+elif typeofop == "s":
+    x=int(raw_input("Enter the first number: "))
+    y=int(raw_input("Enter the second number:"))
+    print x - y
+elif typeofop == "m":
+   x=int(raw_input("Enter the first number: "))
+   y=int(raw_input("Enter the second number:"))
+   print x * y
+elif typeofop == "d":
+    x=int(raw_input("Enter the first number: "))
+    y=int(raw_input("Enter the second number:"))
+    print x / y
+else :
+    print("Wrong input" + typeofop)
