59e6800b03d8d218ba827cd9e00be8e2bba153bc
Aditya Pratti <prattiaditya@gmail.com>
2018-03-03 15:06:19 -0700

    Program to find the Age for Dating

---
+++ Pypractise/Pyscripts/Dating.py
@@ -0,0 +1,7 @@
+# program to find the dating age
+name=raw_input("Enter your name: ")
+age=int(input("Enter your age : "))
+if (age >= 21):
+  print "Ready to date "
+else:
+      print "Not good for dating"
