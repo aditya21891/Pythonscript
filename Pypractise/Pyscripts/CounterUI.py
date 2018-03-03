53cf2ef1f272aaa54594291ec316f6d1b091bca4
Aditya Pratti <prattiaditya@gmail.com>
2018-03-02 17:16:34 -0700

    Tkinter script which increases the counter by1 still stop is pressed

---
+++ Pypractise/Pyscripts/CounterUI.py
@@ -0,0 +1,40 @@
+# A python Tkinter script which increases the counter by1 still stop is pressed.
+import Tkinter as tk
+import tkinter as tk
+
+counter = 0 
+def counter_label(label):
+  def count():
+    global counter
+    counter += 1
+    label.config(text=str(counter))
+    label.after(1000, count)
+  count()
+ 
+ 
+root = tk.Tk()
+root.title("Counting Seconds")
+label = tk.Label(root, fg="green")
+label.pack()
+counter_label(label)
+button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+button.pack()
+root.mainloop()
+counter = 0 
+def counter_label(label):
+  def count():
+    global counter
+    counter += 1
+    label.config(text=str(counter))
+    label.after(1000, count)
+  count()
+ 
+ 
+root = tk.Tk()
+root.title("Counting Seconds")
+label = tk.Label(root, fg="green")
+label.pack()
+counter_label(label)
+button = tk.Button(root, text='Stop', width=25, command=root.destroy)
+button.pack()
+root.mainloop()
