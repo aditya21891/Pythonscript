0e14d718bdcefe4a93a10a59d8d4e545cf13ec03
Aditya Pratti <prattiaditya@gmail.com>
2018-03-08 13:46:46 -0700

    Script to find the abbreviation of US states

---
+++ Pypractise/Pyscripts/Usstateabbrv.py
@@ -0,0 +1,11 @@
+# this python script file is used to retrieve States details with abbreviation
+
+from US import states 
+
+#  define a function to abbreviate the states of United States
+def lookup_state(abbreviation):
+	return abbreviation
+
+
+print lookup_state("NC")
+print lookup_state("CA") 
