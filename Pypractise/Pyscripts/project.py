3d204dddd134c988b5a689c466f015b4138ae197
aditya21891 <prattiaditya@gmail.com>
2019-01-30 08:33:28 -0600

    Update project.py

--- Pypractise/Pyscripts/project.py
+++ Pypractise/Pyscripts/project.py
@@ -1,5 +1,17 @@
-from pytz import timezone 
-import pytz 
-from datetime import datetime 
-from time import strftime 
+from pytz import timezone
+import pytz
+import webbrowser
+import urllib
+
+from datetime import datetime
+from time import strftime
 print(strftime("%H:%M:%S"))
+new=1;
+videourl="https://www.youtube.com/playlist?list=PLzpeeMx5u9u3kvpmO2M8LaueQZT0PKq4M"
+webbrowser.get('safari').open_new(videourl)
+blog=1
+burl="https://medium.com/topic/technology"
+webbrowser.open_new_tab(burl)
+
+
+
